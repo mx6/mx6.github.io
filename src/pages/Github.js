30a7940bdb4787872ee8fe0880007c5413c277f9
M. Adamson <MX@Matthews-MBP.lan>
2021-01-06 12:51:02 -0800

    new git stuff

--- src/pages/Github.js
+++ src/pages/Github.js
@@ -24,6 +24,15 @@ function Github() {
 
                     <div className="card-deck">
 
+                    <div className="card">
+                            <a href="https://enigmatic-garden-49283.herokuapp.com/" target="_blank"><img src="../images/reactreadinglist_th.png" className="card-img-top" alt="This is a full-stack Google Book Search App" /></a>
+                            <div className="card-body">
+                                <h3 className="card-title"><a href="https://enigmatic-garden-49283.herokuapp.com/" target="_blank"> React Reading List App </a></h3>
+                                <p className="card-text">This is a book search app, using the Google Books API and saving the data with Mongoose.</p>
+                                <p>Click <a href="https://github.com/mx6/GoogleBooksSearch">here</a> for the GitHub repository.</p>
+                            </div>
+                        </div>
+
                         <div className="card">
                             <a href="https://project-deux.herokuapp.com/" target="_blank"><img src="../images/copy_cat_th.png" className="card-img-top" alt="Project 2 is a full-stack web application ecommerce site" /></a>
                             <div className="card-body">
@@ -42,14 +51,14 @@ function Github() {
                             </div>
                         </div>
 
-                        <div className="card">
+                        {/* <div className="card">
                             <a href="https://eat-da-b-urger.herokuapp.com/" target="_blank"><img src="../images/burger_app_th.png" className="card-img-top" alt="Screen shot of a burger database application." /></a>
                             <div className="card-body">
                                 <h3 className="card-title"><a href="https://eat-da-b-urger.herokuapp.com/" target="_blank"> Burger App </a></h3>
                                 <p className="card-text">It doesn't look like much, but it's got it where it counts, kid.  This is a full-stack app using handlebars and a database back-end.</p>
                                 <p>Click <a href="https://github.com/mx6/Eat_Da_Burger">here</a> for the GitHub repository.</p>
                             </div>
-                        </div>
+                        </div> */}
 
 
 
@@ -63,6 +72,15 @@ function Github() {
                 <div className="row">
 
                     <div className="card-deck">
+
+                    <div className="card">
+                            <a href="https://eat-da-b-urger.herokuapp.com/" target="_blank"><img src="../images/burger_app_th.png" className="card-img-top" alt="Screen shot of a burger database application." /></a>
+                            <div className="card-body">
+                                <h3 className="card-title"><a href="https://eat-da-b-urger.herokuapp.com/" target="_blank"> Burger App </a></h3>
+                                <p className="card-text">It doesn't look like much, but it's got it where it counts, kid.  This is a full-stack app using handlebars and a database back-end.</p>
+                                <p>Click <a href="https://github.com/mx6/Eat_Da_Burger">here</a> for the GitHub repository.</p>
+                            </div>
+                        </div>
 
                         <div className="card">
                             <a href="https://whispering-woodland-16291.herokuapp.com/" target="_blank"><img src="../images/fitness_tracker.png" className="card-img-top" alt="Simple Workout Tracker App" /></a>
@@ -82,14 +100,14 @@ function Github() {
                             </div>
                         </div>
 
-                        <div className="card">
+                        {/* <div className="card">
                             <a href="https://github.com/mx6/Employee_Tracker" target="_blank"><img src="../images/employee_tracker.png" className="card-img-top" alt="CLI Employee Tracker." /></a>
                             <div className="card-body">
                                 <h3 className="card-title"><a href="https://github.com/mx6/Employee_Tracker" target="_blank"> CLI Employee Tracker </a></h3>
                                 <p className="card-text">A CLI Employee Tracker.  Reminds me of classic 80s movies.</p>
                                 <p>Click <a href="https://github.com/mx6/Employee_Tracker">here</a> for the GitHub repository.</p>
                             </div>
-                        </div>
+                        </div> */}
 
 
 
